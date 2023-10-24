845d582f46fc9cfb6ede86fe99af6cc382607199
Kuldyaev <slavalion1@gmail.com>
2023-10-25 00:20:23 +0300

    Form structure for new task

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import moment from "moment/moment";
 import Task from "./task";
 import TaskTableHeader from "./taskTableHeader";
+import Row from "react-bootstrap/Row";
 import {
   Form,
   Modal,
@@ -13,6 +14,7 @@ import {
   ModalBody,
   ModalHeader,
   ModalTitle,
+  Col,
 } from "react-bootstrap";
 
 const Main = function Main() {
@@ -25,12 +27,29 @@ const Main = function Main() {
     !isAuthorized && navigate("/login");
   }, []);
 
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = {
+      title: form.title.value,
+      description: form.description.value,
+      priority: form.priority.value,
+      status: form.status.value,
+      executor: form.executor.value,
+      creator: 'current user',
+      date_end: form.date_end.value,
+    };
+    console.log(formData);
+  };
+
   return (
     <main>
       <div className="tasksDeskHeader">
         <h2 id="formtitle">
-          <Badge text="dark">{'сегодня ' + moment().format("DD-MM-YYYY")}</Badge>
-        </h2>  
+          <Badge text="dark">
+            {"сегодня " + moment().format("DD-MM-YYYY")}
+          </Badge>
+        </h2>
         <h2 id="formtitle">
           <Badge bg="secondary">Список задач:</Badge>
         </h2>
@@ -53,39 +72,89 @@ const Main = function Main() {
           <ModalTitle>Новая задача</ModalTitle>
         </ModalHeader>
         <ModalBody>
-          <Form>
+          <Form onSubmit={handleSubmitForm}>
             <FormGroup controlId="title">
               <Form.Label>Заголовок</Form.Label>
-              <Form.Control type="text" placeholder="Заголовок для задачи" />
+              <Form.Control
+                type="text"
+                placeholder="Заголовок для задачи"
+                required
+              />
             </FormGroup>
             <FormGroup controlId="description">
               <Form.Label>Описание</Form.Label>
               <Form.Control
-                type="text"
+                as="textarea"
                 placeholder="Краткое описание для задачи"
+                required
               />
             </FormGroup>
-            <FormGroup className="horizontalBlock">
-              <FormGroup controlId="priority">
-                <Form.Label>Приоритет</Form.Label>
-                <Form.Control type="text" placeholder="приоритет задачи" />
-              </FormGroup>
-              <FormGroup controlId="status">
-                <Form.Label>Статус</Form.Label>
-                <Form.Control type="text" placeholder="статус задачи" />
-              </FormGroup>
+            <FormGroup controlId="executor">
+              <Form.Label>Исполнитель</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Назначенный исполнитель для задачи"
+                required
+              />
             </FormGroup>
+            <Row className="mb-3">
+              <FormGroup
+                as={Col}
+                md="5"
+                controlId="priority"
+                className="horizontalBlockItem"
+              >
+                <Form.Label>Приоритет</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="приоритет задачи"
+                  required
+                />
+              </FormGroup>
+              <FormGroup
+                as={Col}
+                md="5"
+                controlId="status"
+                className="horizontalBlockItem"
+              >
+                <Form.Label>Статус</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="статус задачи"
+                  required
+                />
+              </FormGroup>
+              <FormGroup
+                as={Col}
+                md="5"
+                controlId="date_end"
+                className="horizontalBlockItem"
+              >
+                <Form.Label>Дата окончания</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="дата завершения"
+                  required
+                />
+              </FormGroup>
+            </Row>
+            <Row className="mb-2">
+              <FormGroup md="5" as={Col}>
+              <Button
+                variant="secondary"
+                onClick={() => setNewTaskModalShowed(false)}
+              >
+                Отмена
+              </Button>
+              </FormGroup>
+              <FormGroup md="5" as={Col}> 
+              <Button variant="primary"  type="submit">
+                Создать задачу
+              </Button>
+              </FormGroup>
+            </Row>
           </Form>
         </ModalBody>
-        <Modal.Footer>
-          <Button
-            variant="secondary"
-            onClick={() => setNewTaskModalShowed(false)}
-          >
-            Отмена
-          </Button>
-          <Button variant="primary">Создать задачу</Button>
-        </Modal.Footer>
       </Modal>
     </main>
   );
